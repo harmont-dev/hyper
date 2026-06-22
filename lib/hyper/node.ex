30a9970204efda3cb3180693de4f268462541476
Marko Vejnovic <markove2000@gmail.com>
2026-06-22 04:51:59 +0000

    feat(node): start_image_vm/stop_image_vm orchestration

--- lib/hyper/node.ex
+++ lib/hyper/node.ex
@@ -31,6 +31,11 @@ defmodule Hyper.Node do
   use Supervisor
   use OpenTelemetryDecorator
 
+  alias Hyper.Node.FireVMM
+  alias Hyper.Node.Img
+  alias Hyper.Node.Users
+  alias Hyper.Node.Vmlinux
+
   @vm_sup Hyper.Node.VMSupervisor
 
   def start_link(opts \\ []) do
@@ -53,11 +58,62 @@ defmodule Hyper.Node do
     Supervisor.init(children, strategy: :one_for_one)
   end
 
+  @doc """
+  Boot an image-backed VM on this node: claim a uid, build the writable rootfs,
+  resolve the kernel, and start the VM supervisor. The uid is freed and the
+  writable layer torn down automatically when the VM supervisor dies.
+  """
+  @spec start_image_vm(map()) :: {:ok, pid()} | {:error, term()}
+  @decorate with_span("Hyper.Node.start_image_vm", include: [:params])
+  def start_image_vm(%{vm_id: _vm_id, img_id: _img_id, arch: _arch} = params) do
+    with {:ok, uid} <- Users.claim(),
+         {:ok, writable} <- start_writable_or_release(params[:img_id], params[:vm_id], uid),
+         dev = Img.Writable.blk_path(writable),
+         kernel = Vmlinux.path(params[:arch]),
+         opts = build_opts(params, uid, dev, kernel),
+         {:ok, pid} <- start_vm_or_release(opts, uid, writable) do
+      # Bind the uid and the writable layer to the VM supervisor's lifetime.
+      :ok = Users.bind(uid, pid)
+      :ok = Img.Writable.acquire(writable, pid)
+      :ok = Img.Writable.release(writable)
+      {:ok, pid}
+    end
+  end
+
+  @doc "Tear down an image-backed VM started by `start_image_vm/1`."
+  @spec stop_image_vm(pid()) :: :ok
+  def stop_image_vm(pid) do
+    case DynamicSupervisor.terminate_child(@vm_sup, pid) do
+      :ok -> :ok
+      {:error, :not_found} -> :ok
+    end
+  end
+
+  @doc false
+  @spec build_opts(map(), Users.id(), Path.t(), Path.t()) :: FireVMM.Opts.t()
+  def build_opts(%{vm_id: vm_id, type: type, arch: arch} = params, uid, dev, kernel) do
+    source =
+      %{
+        kernel_image_path: kernel,
+        root_drive_path: dev
+      }
+      |> maybe_put(:boot_args, Map.get(params, :boot_args))
+
+    %FireVMM.Opts{
+      vm_id: vm_id,
+      uid: uid,
+      gid: uid,
+      type: type,
+      arch: arch,
+      source: source
+    }
+  end
+
   @doc "Start a microVM on this node."
-  @spec start_vm(Hyper.Node.FireVMM.opts()) :: DynamicSupervisor.on_start_child()
+  @spec start_vm(FireVMM.Opts.t()) :: DynamicSupervisor.on_start_child()
   @decorate with_span("Hyper.Node.start_vm", include: [:opts])
-  def start_vm(%{id: _} = opts) do
-    DynamicSupervisor.start_child(@vm_sup, {Hyper.Node.FireVMM, opts})
+  def start_vm(%FireVMM.Opts{} = opts) do
+    DynamicSupervisor.start_child(@vm_sup, {FireVMM, opts})
   end
 
   @doc """
@@ -99,6 +155,35 @@ defmodule Hyper.Node do
          :ok <- Hyper.Node.Layer.Repo.test_system(),
          :ok <- Sys.Linux.Dmsetup.test_system() do
       Hyper.Node.FireVMM.test_system()
+    end
+  end
+
+  defp maybe_put(map, _k, nil), do: map
+  defp maybe_put(map, k, v), do: Map.put(map, k, v)
+
+  # Acquire the writable on our own pid initially so it does not idle-reap during
+  # boot; release on failure so it tears down.
+  defp start_writable_or_release(img_id, vm_id, uid) do
+    case Img.start_writable(img_id, vm_id) do
+      {:ok, writable} ->
+        :ok = Img.Writable.acquire(writable)
+        {:ok, writable}
+
+      {:error, reason} ->
+        Users.release(uid)
+        {:error, reason}
+    end
+  end
+
+  defp start_vm_or_release(opts, uid, writable) do
+    case start_vm(opts) do
+      {:ok, pid} ->
+        {:ok, pid}
+
+      {:error, reason} ->
+        Img.Writable.release(writable)
+        Users.release(uid)
+        {:error, reason}
     end
   end
 end
