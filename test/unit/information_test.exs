3078a754f754523d7b64bfb23247db6cd3410408
Marko Vejnovic <marko@harmont.dev>
2026-06-24 01:09:48 +0000

    test: cover Unit.Information constructors and truncating accessors

--- test/unit/information_test.exs
+++ test/unit/information_test.exs
@@ -24,4 +24,24 @@ defmodule Unit.InformationTest do
   test "zero is the additive identity" do
     assert Information.zero() + Information.gib(1) == Information.gib(1)
   end
+
+  describe "constructors and accessors" do
+    test "binary-prefix constructors scale by powers of 1024" do
+      assert Information.as_bytes(Information.kib(1)) == 1024
+      assert Information.as_bytes(Information.mib(1)) == 1024 * 1024
+      assert Information.as_bytes(Information.gib(1)) == 1024 * 1024 * 1024
+      assert Information.as_bytes(Information.tib(1)) == 1024 * 1024 * 1024 * 1024
+    end
+
+    test "each prefix is 1024x the prefix below it" do
+      assert Information.kib(1024) == Information.mib(1)
+      assert Information.mib(1024) == Information.gib(1)
+      assert Information.gib(1024) == Information.tib(1)
+    end
+
+    test "as_mib and as_gib truncate toward zero" do
+      assert Information.as_mib(Information.mib(1) + Information.kib(512)) == 1
+      assert Information.as_gib(Information.gib(2) + Information.mib(900)) == 2
+    end
+  end
 end
