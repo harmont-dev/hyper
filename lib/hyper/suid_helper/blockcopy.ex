3564965d01483bbb2ba4d50650e4f761aeddc62d
Marko Vejnovic <marko@harmont.dev>
2026-07-07 19:51:31 +0000

    feat(suid_helper): thin-dump wrapper and ranged blockcopy option

--- lib/hyper/suid_helper/blockcopy.ex
+++ lib/hyper/suid_helper/blockcopy.ex
@@ -11,12 +11,14 @@ defmodule Hyper.SuidHelper.Blockcopy do
 
   @type err :: SuidHelper.err()
 
-  @doc "Copy `src` into `dst`; `opts[:reference]` skips chunks equal to it."
+  @doc "Copy `src` into `dst`; `opts[:reference]` skips chunks equal to it; `opts[:ranges]` copies only the block ranges in the given JSON file (from `ThinDump.mappings/2`)."
   @spec copy(Path.t(), Path.t(), keyword()) ::
           {:ok, %{scanned: non_neg_integer(), written: non_neg_integer()}} | {:error, err()}
   @decorate with_span("Hyper.SuidHelper.Blockcopy.copy", include: [:src, :dst])
   def copy(src, dst, opts \\ []) do
-    argv = ["blockcopy", "--src", src, "--dst", dst] ++ reference_args(opts[:reference])
+    argv =
+      ["blockcopy", "--src", src, "--dst", dst] ++
+        reference_args(opts[:reference]) ++ ranges_args(opts[:ranges])
 
     case SuidHelper.exec(argv) do
       {:ok, %{"scanned_chunks" => scanned, "written_chunks" => written}} ->
@@ -30,4 +32,8 @@ defmodule Hyper.SuidHelper.Blockcopy do
   @spec reference_args(Path.t() | nil) :: [String.t()]
   defp reference_args(nil), do: []
   defp reference_args(reference), do: ["--reference", reference]
+
+  @spec ranges_args(Path.t() | nil) :: [String.t()]
+  defp ranges_args(nil), do: []
+  defp ranges_args(path), do: ["--ranges", path]
 end
